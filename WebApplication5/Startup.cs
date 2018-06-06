0c811688b300c4dab91b3194886025630c1db096
APAC\kham441 <maulikk2000@gmail.com>
2018-06-07 01:33:01 +1000

    added loggerfactory

--- WebApplication5/Startup.cs
+++ WebApplication5/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace WebApplication5
 {
@@ -36,7 +37,7 @@ namespace WebApplication5
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             if (env.IsDevelopment())
             {
